cf4e6971f4102ac56dafb9597615dbae48448c6d
Unknown <DavidJGalazin@gmail.com>
2017-09-24 08:40:23 +0100

    Added a few comments

--- Extension/js/popup.js
+++ Extension/js/popup.js
@@ -1,3 +1,9 @@
+// This is the JS for the options seen by clicking the extension icon.
+// For the time being it is simply a toggle for on/off however
+// I plan upon expanding this section in the future to allow things
+// such as theme selection...
+
+// Basic save function I pulled in from Google's tutorial
 function save_options() {
     var state = document.getElementById('state').checked;
     chrome.storage.sync.set({
@@ -5,6 +11,7 @@ function save_options() {
     });
 }
 
+// Function to retrieve the setting from the user.
 function restore_options() {
     chrome.storage.sync.get({
         state: true
@@ -12,6 +19,8 @@ function restore_options() {
         document.getElementById('state').checked = items.state;
     });
 }
+
+// Add event listeners for the abover functions
 document.addEventListener('DOMContentLoaded', restore_options);
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('state').addEventListener('click', save_options);
--- Extension/js/reddit.js
+++ Extension/js/reddit.js
@@ -2,11 +2,18 @@ chrome.storage.sync.get({
     state: true
 }, function (items) {
     if (items.state) {
-        // set up the mutation observer
+        // Set up a mutation observer to remove subreddit style
+        // `mutations` is an array of mutations that occurred
+        // `me` is the MutationObserver instance
         var observerCss = new MutationObserver(function (mutations, me) {
-            // `mutations` is an array of mutations that occurred
-            // `me` is the MutationObserver instance
+            // Query selector grabs link tag with subreddit style
             var style = document.querySelector("link[ref=applied_subreddit_stylesheet]");
+            // If it finds the tag i.e. the subreddit has its own stylesheet then
+            // it removes the tag and inserts a new tag referencing the extensions
+            // own CSS file. I realise I can have the extension insert the CSS
+            // automatically rather than adding a reference but that cause issues
+            // as the page would render prior to the extension injecting the CSS.
+            // This was the only method I found that works.
             if (style) {
                 style.parentNode.removeChild(style);
                 //document.querySelector('head').innerHTML += '<link rel="stylesheet" href=' + chrome.extension.getURL('/reddit.css') + ' type="text/css"/>';
@@ -16,18 +23,24 @@ chrome.storage.sync.get({
             }
         });
 
-        // start observing
+        // Start observing
         observerCss.observe(document, {
             childList: true,
             subtree: true
         });
 
 
-        // set up the mutation observer
+        // Set up another mutation observer to eddit the reddit logo
+        // `mutations` is an array of mutations that occurred
+        // `me` is the MutationObserver instance
         var observer = new MutationObserver(function (mutations, me) {
-            // `mutations` is an array of mutations that occurred
-            // `me` is the MutationObserver instance
+            // Grab reddit logo in the top banner
             var img = document.getElementById('header-img');
+            // If present then adjust the a tag to the correct dimensions
+            // and alter the image source to the extensions reddit logo.
+            // The reason the dimensions need to be set here is that some subreddits
+            // have custom logos and I found it necessary to hard code the dimensions
+            // to prevent the logo the extension uses from warping the page.
             if (img) {
                 document.getElementById("header-img").width = 39;
                 document.getElementById("header-img").height = 54;
@@ -37,7 +50,7 @@ chrome.storage.sync.get({
             }
         });
 
-        // start observing
+        // Start observing
         observer.observe(document, {
             childList: true,
             subtree: true
