32ae56694eeecaf525785811141d20723bfb21d7
Unknown <DavidJGalazin@gmail.com>
2017-09-25 12:05:16 +0100

    Clean up web_accessible_resources referencing and alter extension to work on all of reddit and not just community owned subreddits...

--- Unpacked Extension/js/reddit.js
+++ Unpacked Extension/js/reddit.js
@@ -6,21 +6,19 @@ chrome.storage.sync.get({
         // `mutations` is an array of mutations that occurred
         // `me` is the MutationObserver instance
         var observerCss = new MutationObserver(function (mutations, me) {
+            console.log("CSS Mutation Observer");
             // Query selector grabs link tag with subreddit style
             var style = document.querySelector("link[ref=applied_subreddit_stylesheet]");
-            // If it finds the tag i.e. the subreddit has its own stylesheet then
-            // it removes the tag and inserts a new tag referencing the extensions
-            // own CSS file. I realise I can have the extension insert the CSS
-            // automatically rather than adding a reference but that cause issues
+            if (style) style.parentNode.removeChild(style);
+            // If a subreddit style sheet is specified then it is removed.
+            // The custom CSS file is inserted/appended to the head.
+            $('head').append('<link rel="stylesheet" href="' + chrome.extension.getURL('/css/reddit.css') + '" type="text/css" />');
+            // I realise I can have the extension insert the CSS
+            // automatically rather than adding a reference but that caused issues
             // as the page would render prior to the extension injecting the CSS.
             // This was the only method I found that works.
-            if (style) {
-                style.parentNode.removeChild(style);
-                //document.querySelector('head').innerHTML += '<link rel="stylesheet" href=' + chrome.extension.getURL('/reddit.css') + ' type="text/css"/>';
-                $('head').append('<link rel="stylesheet" href="' + chrome.extension.getURL('/reddit.css') + '" type="text/css" />');
-                me.disconnect(); // stop observing
-                return;
-            }
+            me.disconnect(); // stop observing
+            return;
         });
 
         // Start observing
@@ -30,13 +28,14 @@ chrome.storage.sync.get({
         });
 
 
+
         // Set up another mutation observer to eddit the reddit logo
         // `mutations` is an array of mutations that occurred
         // `me` is the MutationObserver instance
         var observer = new MutationObserver(function (mutations, me) {
             // Grab reddit logo in the top banner
             var img = document.getElementById('header-img');
-            // If present then adjust the a tag to the correct dimensions
+            // If present then adjust the img tag to the correct dimensions
             // and alter the image source to the extensions reddit logo.
             // The reason the dimensions need to be set here is that some subreddits
             // have custom logos and I found it necessary to hard code the dimensions
